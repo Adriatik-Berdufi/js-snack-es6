6cc697e3cf765df64ec5e153c2a16b99226d01e9
Adriatik Berdufi <adratik.berdufi@gmail.com>
2024-01-24 14:48:26 +0100

    snack 1

--- Snack1.js
+++ Snack1.js
@@ -11,3 +11,14 @@ const vips = [
     'Amal Clooney',
     'Maneskin',
   ];
+
+  let formatoTipografia =  vips.map((invitato ,index) => {
+    invitato = {
+      nomeTavolo : "Tavolo Vip",
+      nomeInvitato : invitato ,
+      nrPosto : index + 1
+    }
+    return invitato;
+
+  });
+  console.table(formatoTipografia);
